8c61f6b68a0989132a9ce4fab8338bf1ebe1eff6
bkfleet1 <bkfleet1@gmail.com>
2022-01-04 18:25:34 -0600

    ajax library update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
 
 //Ticketmaster API call to pull genres
 $.ajax({
-    type:"GET",
-    url:"https://app.ticketmaster.com/discovery/v2/classifications/segments/KZFzniwnSyZfZ7v7nJ?apikey=mN8PQ731bAnsxgiKstMF7PWhVZtHxsEA",
-    async:true,
-    dataType: "json",
-    success: function(data) {
-       const genres = data._embedded.genres;
-                console.log(genres);
-             },
-    error: function(xhr, status, err) {
-             }
-  });
+   type: "GET",
+   url: "https://app.ticketmaster.com/discovery/v2/classifications/segments/KZFzniwnSyZfZ7v7nJ?apikey=mN8PQ731bAnsxgiKstMF7PWhVZtHxsEA",
+   async: true,
+   dataType: "json",
+   success: function (data) {
+      const genres = data._embedded.genres;
+      console.log(genres);
+         
+      
+   },
+   error: function (xhr, status, err) {
+   }
+});
+
