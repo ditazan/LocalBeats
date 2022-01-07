53d2ee1384b341d57b9f272b4d2d4a1b66693a1d
bkfleet1 <bkfleet1@gmail.com>
2022-01-06 18:41:11 -0600

    API populating dropdown and form submission returning event data

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,31 +10,52 @@ $.ajax({
       for (let i = 0; i < genres.length; i++) {
          const genreIds = genres[i].id;
          const genreNames = genres[i].name;
-         console.log(genreIds, genreNames);
+         appendGenres(genreIds, genreNames);
       }
    },
    error: function (xhr, status, err) {
    }
 });
 
+function appendGenres(genreIds, genreNames) {
+   const genreInitial = $('<option id="genreNone" value=""></option>').text("");
+   const genreOptions = $('<option id="' + genreIds + '" value="' + genreIds + '"></option>').text(genreNames);
+   $("#genreId").append(genreInitial, genreOptions);
+}
 
-const genreId = "KnvZfZ7vAeA"; //rock genre
-const radius = "500"; // radius does not appear to be functioning
-const postalCode = "28202"; // postal code appears to be specific to the venue
-const city = "Houston"
-const size = "10";
-const status = "onsale"
-// ticketmaster events api call
-$.ajax({
-   type: "GET",
-   url: "https://app.ticketmaster.com/discovery/v2/events.json?apikey=mN8PQ731bAnsxgiKstMF7PWhVZtHxsEA&size=" + size + "&genreId=" + genreId + "&city=" + city + "&radius=" + radius + "&unit=miles",
-   async: true,
-   dataType: "json",
-   success: function (eventsData) {
-      const events = eventsData._embedded.events;
-      console.log(events);
-   },
-   error: function (xhr, status, err) {
-
+$('.submit').on('click', function (event) {
+   event.preventDefault();
+   const cityName = $("#cityName").val();
+   const genreId = $("#genreId").val();
+   if (cityName == null || cityName == "") {
+      alert("ERROR: A city name is required. Please enter a city name.")
    }
+   else
+      //  console.log(cityName,genreId);
+
+      $.ajax({
+         type: "GET",
+         url: "https://app.ticketmaster.com/discovery/v2/events.json?apikey=mN8PQ731bAnsxgiKstMF7PWhVZtHxsEA&size=20&genreId=" + genreId + "&city=" + cityName + "&radius=100&unit=miles",
+         async: true,
+         dataType: "json",
+         success: function (eventsData) {
+            const events = eventsData._embedded.events;
+            console.log(events);
+            for (let i = 0; i < events.length; i++) {
+               const eventDate = events[i].dates.start.localDate;
+               const eventTime = events[i].dates.start.localTime;
+               const eventTimeZone = events[i].dates.timezone;
+               const eventStatus = events[i].dates.status.code;
+               const eventName = events[i].name;
+               const eventVenue = events[i]._embedded.venues;
+               console.log(eventDate, eventTime, eventTimeZone, eventStatus, eventName);
+               console.log(eventVenue);
+            }
+
+         },
+         error: function (xhr, status, err) {
+
+         }
+      })
+
 });
