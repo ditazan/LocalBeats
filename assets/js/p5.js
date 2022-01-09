1bddc8df65d78d1c99376b536b5af50f75558be3
buykajamsran <buyka.jamsran@gmail.com>
2022-01-09 15:43:36 -0600

    updated the p5 to fit into the framework in the background

--- assets/js/p5.js
+++ assets/js/p5.js
@@ -1,12 +1,13 @@
-var canvas; 
 let totalPts = 300;
 let steps = totalPts + 1;
 
+var canvas;
 function setup() {
-  canvas = createCanvas(710, 400);
-  canvas.position(0,0);
+  canvas = createCanvas(1500, 900);
+  canvas.position(0, 0);
+  canvas.style('z-index', '-1');
   stroke(255);
-  frameRate(1);
+  frameRate(10);
 }
 
 function draw() {
@@ -14,6 +15,6 @@ function draw() {
   let rand = 0;
   for (let i = 1; i < steps; i++) {
     point((width / steps) * i, height / 2 + random(-rand, rand));
-    rand += random(-5, 5);
+    rand += random(-10, 10);
   }
 }
